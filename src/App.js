3cc68d54dc145724702f938d2228675c0688d6fe
alptugozates <omeralptugozates@gmail.com>
2023-11-08 14:39:22 +0300

    Router eklendi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
-import Header from "../src/Components/Header";
-
+import Home from "../src/Components/Home";
+import Order from "../src/Components/Order";
+import { Route, Switch } from "react-router-dom";
+import SiparisOzet from "./Components/SiparisOzet";
 const App = () => {
   return (
     <>
-      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/pizza" component={Order} />
+        <Route path="/siparis-ozeti" component={SiparisOzet} />
+      </Switch>
     </>
   );
 };
