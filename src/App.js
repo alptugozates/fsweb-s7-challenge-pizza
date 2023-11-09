cc553f56299c7ccf43a2c44bcce6be0a13995da6
alptugozates <omeralptugozates@gmail.com>
2023-11-09 16:08:44 +0300

    Ayrı bir state oluşturup  datalar aktarıldı

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "../src/Components/Home";
 import Order from "../src/Components/Order";
 import { Route, Switch } from "react-router-dom";
 import SiparisOzet from "./Components/SiparisOzet";
+
+
+
 const App = () => {
+
+  const [siparis, setSiparis] = useState({});
+
   return (
     <>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/pizza" component={Order} />
-        <Route path="/siparis-ozeti" component={SiparisOzet} />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/pizza">
+          <Order setSiparis={setSiparis} />
+        </Route>
+        <Route path="/siparis-ozeti">
+          <SiparisOzet siparis={siparis} />
+        </Route>
+
       </Switch>
     </>
   );
